3c2c8517929d37abdae3204c930e205a15663e60
Jennifer Bach <jmbach1@gmail.com>
2018-07-04 16:21:03 -0700

    added code, whiteboard, readme and tests

---
+++ __test__/sort/quickSort.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let quickSort = require('../../sort/quickSort/quickSort.js');
+
+describe('quickSort', () => {
+  it('returns array if length of 1', () => {
+    let outcome = quickSort([2]);
+    expect(outcome).toEqual([2]);
+  });
+
+  it('returns array no length', () => {
+    let outcome = quickSort([]);
+    expect(outcome).toEqual([]);
+  });
+
+  it('quickSort() sorts array correctly', () => {
+    let outcome = quickSort([1,6,9,10,3,5,-10]);
+    expect(outcome).toEqual([-10, 1, 3, 5, 6, 9, 10]);
+  });
+
+});
