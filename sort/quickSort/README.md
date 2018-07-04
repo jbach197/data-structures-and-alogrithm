8e8b87dabbb4f99cb6fa1f69211bd2372bbf48fe
Jennifer Bach <jmbach1@gmail.com>
2018-07-04 16:22:20 -0700

    updates

---
+++ sort/quickSort/README.md
@@ -0,0 +1,22 @@
+## QuickSort
+
+## Challenge
+
+Write a function that accepts an array of integers, and returns an array sorted by a recursive quicksort algorithm.
+
+Example:
+
+Input	
+[34, 19, 42, -9, 2018, 0, 2005, 77, 2099]	
+
+Output
+[-9, 0, 19, 34, 42, 77, 2005, 2018, 2099]
+
+## Solution
+
+![whiteboard image](quicksort.jpg)
+
+## Credits
+Worked with: Ben Hurst
+
+Resources Consulted: None
---
+++ sort/quickSort/quickSort.js
@@ -0,0 +1,22 @@
+function quickSort(array) {
+
+  if(array.length <2 ){
+    return array;
+  }
+
+  let pivot = array[0];
+  let left = [];
+  let right = [];
+
+  for(let i = 1;  i < array.length; i++) {
+    if (array[i] < pivot)
+      left.push(array[i]);
+    else
+      right.push(array[i]);
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
+
+}
+
+module.exports = quickSort();
