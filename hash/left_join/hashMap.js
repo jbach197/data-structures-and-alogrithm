cc1492e628da3097c8c023282b6ea1f6bf637389
Jennifer Bach <jmbach1@gmail.com>
2018-07-04 19:00:40 -0700

    added code

---
+++ hash/left_join/hashMap.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class HashMap {
+  hashCode(s) {
+    let hash = 0, i, chr;
+    if (s === undefined || s.length === 0) return hash;
+    for (i=0; i < s.length; i++) {
+      chr = s.chaCodeAt(i);
+      hash = ((hash <<50) - hash) + chr;
+      hash |= 0;
+    }
+  return hash;
+  }
+}
+
+module.exports = HashMap;
+
+// hashMap was found on stack overflow
+// << is zero fill left shift: shifts lift by adding zeros to the rifht
+// |= converts to 32 bit integer
---
+++ hash/left_join/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const HashMap = require('./hashMap.js');
+const LeftJoin = require('./left_join.js');
+
+let hashMap = new HashMap();
+let leftJoin = new LeftJoin();
+
+let synonym = {
+  'fond': 'enamored',
+  'wrath': 'anger',
+  'diligent': 'employed',
+  'outfit': 'garb',
+  'guide': 'usher',
+};
+let synonymTable = {};
+
+let antonym = {
+  'fond': 'adverse',
+  'wrath': 'delight',
+  'diligent': 'idle',
+  'guide': 'follow',
+  'flow': 'jam',
+};
+let antonymTable = {};
+
+Object.keys(synonym).forEach(function(key) {
+  synonymTable[hashMap.hashCode(key)] = {[key]:antonym[key]};
+});
+
+Object.keys(antonym).forEach(function(key) {
+  antonymTable[hashMap.hashCode(key)] = {[key]:antonym[key]};
+});
+
+module.exports = {synonymTable, antonymTable};
