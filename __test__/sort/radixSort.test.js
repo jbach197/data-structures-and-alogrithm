684742305f7fd10d6786ad4e6f331b266bb44d74
Jennifer Bach <jmbach1@gmail.com>
2018-07-06 20:07:27 -0700

    added tests

---
+++ __test__/sort/radixSort.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let radixSort = require('../../sort/radixSort/radixSort.js');
+
+describe('radixSort', () => {
+  it('radixSort() returns original array if 1 element', () => {
+    let outcome = radixSort([2]);
+    expect(outcome).toEqual([2]);
+  });
+
+  it('radixSort() sorts correctly', () => {
+    let outcome = radixSort([1,6,20,7,2]);
+    expect(outcome).toEqual([ 1, 6, 20, 7, ]);
+  });
+
+  it('radixSort() sorts array correctly', () => {
+    let outcome = radixSort([1,6,2018,20,7,2,500]);
+    expect(outcome).toEqual([ 1, 6, 2018, 20, 7, 2, 500 ]);
+  });
+
+});
